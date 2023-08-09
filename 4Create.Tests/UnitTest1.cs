2a828cb574c9685c318869bb481b5e686c79a7c7
filipzdelar <filipzdelar@gmail.com>
2023-08-09 11:20:51 +0200

    microservices arhitecutre

---
+++ 4Create.Tests/UnitTest1.cs
@@ -0,0 +1,24 @@
+using _4Create.Controllers;
+using _4Create.Entities.Dtos;
+using _4Create.Entities.Models;
+using _4Create.Services.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Moq;
+
+namespace _4Create.Tests
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+
+        [TestMethod]
+        public async Task CreateEmployeeAsync_ValidData_ReturnsCreatedAtAction()
+        {
+        }
+    }
+}
