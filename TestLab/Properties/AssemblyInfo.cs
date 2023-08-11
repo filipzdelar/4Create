572eaaf92a060833f0459bc870694f5bd3ec6616
filipzdelar <filipzdelar@gmail.com>
2023-08-11 22:47:14 +0200

    Microservice

---
+++ TestLab/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestLab")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("TestLab")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("bb4a6375-6784-44c9-9481-f8d144544ad4")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestLab/UnitTest1.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using _4Create.Entities.Models;
+
+namespace TestLab
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var testProducts = ();
+            var controller = new SimpleProductController(testProducts);
+
+            var result = controller.GetAllProducts() as List<Product>;
+            Assert.AreEqual(testProducts.Count, result.Count);
+        }
+    }
+}
